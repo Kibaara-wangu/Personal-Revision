322632838a8a6ae41136269f2e7e8c649306c2a0
kibaara-wangu <kibaarawangu@gmail.com>
2024-03-11 21:22:02 +0300

    add files

---
+++ index.js
@@ -0,0 +1,75 @@
+// Write a JavaScript expression using the modulus operator to find the remainder when 15 is divided by 4.
+let x = 15;
+let y = 4;
+let modulus = x % 4;
+console.log(modulus);
+
+
+// What is the difference between the "==" and "===" operators in JavaScript? Provide an example.
+// "==" represent looselyEqualTo in Javascript. It only cheecks for the value.
+// "===" represents strictlyEqualTo. It checks for both values and datatypes.
+let m = "4";
+let looselyEqualTo = y == m;
+console.log(looselyEqualTo);
+
+let strictlyEqualTo = y ===m;
+console.log(strictlyEqualTo);
+
+// Write a JavaScript expression to concatenate two strings.
+let j = "my";
+let k = "valentine";
+let identity = j +" "+k;
+console.log(identity);
+
+
+
+// Explain the difference between null and undefined in JavaScript.
+// undefined means that a variable has already been declared but hasn't been assigned a value while Null means that a variable has been declared a value by null.
+
+// Create an array in JavaScript and demonstrate how to access the third element.
+const fruits = ["orange", "pineapple", "mango"];
+let fruit = fruits[2];
+
+
+//Calculate the result of 8 divided by 3 using both regular division and floor division in Python.
+e = 12;
+p = 4;
+division = e / p
+print(division)
+
+e= 12
+p=5
+print(12//5)
+
+
+// Explain the use of the in operator in Python with a practical example
+// It is used to check if a value exist within a string
+"r" in "flower" 
+// output s = 11
+
+// Write a Python expression to find the product of two numbers.
+q = 8
+m = 3
+d = q * m
+
+// Declare a variable in Python and assign it the value of a floating-point number. Determine its data type using the type() function.
+i= 35.3
+typeof(i)
+
+
+
+
+// What is the difference between a list and a tuple in Python? Provide an example of each.
+// Turples are immutable meaning they can't be reassigned a value ones assigned while lite is mutable meaning you can assighn another value to it.
+// List are a group of comma seperated values within a square brackets while turple is a group of separated value within parenthesis
+eg m[20,4,12,34]
+k(12,48,54)
+
+// Create a dictionary in Python representing a person with keys for "name," "age," and "city."
+// thisdict = dict(name="mercy", age=26, city="Nairobi")
+// print(thisdict)
+
+
+
+
+
