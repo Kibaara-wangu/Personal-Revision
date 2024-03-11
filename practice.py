821851c755ebf5c8b6305057ff7cda08a998d346
kibaara-wangu <kibaarawangu@gmail.com>
2024-03-11 23:01:18 +0300

    Personal work for Js and Python

---
+++ practice.py
@@ -0,0 +1,39 @@
+#Calculate the result of 8 divided by 3 using both regular division and floor division in Python.
+e = 12;
+p = 4;
+division = e / p
+print(division)
+
+e= 12
+p=5
+print(12//5)
+
+
+#Explain the use of the in operator in Python with a practical example
+#It is used to check if a value exist within a string
+"r" in "flower" 
+// output s = 11
+
+#Write a Python expression to find the product of two numbers.
+q = 8
+m = 3
+d = q * m
+
+# Declare a variable in Python and assign it the value of a floating-point number. Determine its data type using the type() function.
+i= 35.3
+typeof(i)
+
+
+
+
+#What is the difference between a list and a tuple in Python? Provide an example of each.
+#Turples are immutable meaning they can't be reassigned a value ones assigned while lite is mutable meaning you can assighn another value to it.
+#List are a group of comma seperated values within a square brackets while turple is a group of separated value within parenthesis
+eg m[20,4,12,34]
+k(12,48,54)
+
+#Create a dictionary in Python representing a person with keys for "name," "age," and "city."
+thisdict = dict(name="mercy", age=26, city="Nairobi")
+print(thisdict)
+
+
