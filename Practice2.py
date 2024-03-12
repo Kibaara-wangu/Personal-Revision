a4bdad2d965246bea0ee6b33443c2a03020f134d
kibaara-wangu <kibaarawangu@gmail.com>
2024-03-12 11:56:10 +0300

    python practice

---
+++ Practice2.py
@@ -0,0 +1,77 @@
+# Use string formatting to create a message that includes variables for a person's name and age. Print the formatted message.
+name = "Jane"
+age = 22
+sentence = f"My name is {name} and i am {age} years old"
+print(sentence)
+
+#Explain the difference between `%` formatting and `str.format()` for string formatting in Python.
+
+
+
+
+#  Given the string `"Python"`, retrieve and print the first and last characters using indexing.
+p = "Python"
+print(p[o])
+print(p[5])
+
+# Determine the length of the string `"Hello, World!"` in Python.
+words="Hello, World"
+print(len(name))
+
+#Given the string `"Programming"`, use slicing to extract the substring "gram."
+word="Programming"
+print(word[3:7])
+
+# Slice the string `"abcdefgh"` to create a new string that includes only the characters from index 2 to index 5.
+letters = "abcdefgh"
+print(letters[2:5])
+
+# Use the `count()` method to find the number of occurrences of the letter "a" in the string `"banana"`.
+fruit = "banana"
+print(fruit.count("a"))
+
+
+# Convert the string `"hello"` to uppercase using the `upper()` method.
+word = "hello"
+print(word.upper())
+
+# Replace all occurrences of the word "apple" with "orange" in the string `"I have an apple and another apple."`
+Sentence = "I have an apple and another apple"
+print(sentence.replace("apple","orange"))
+
+
+# Concatenate the strings "Hello" and "World" to create the sentence "Hello World" in Python.
+Greeting = "Hello"
+name = "World"
+sentence= {Greeting} " " {name}
+print(sentence)
+
+# Given the string `"Python is fun"`, check if the string starts with "Python" using the `startswith()` method.
+sentence = "python is fun"
+print(sentence.startswith("p"))
+
+#  Convert the number `42` to a string in Python.
+k = 42
+k ="42"
+type(k)
+
+
+#  Convert the string `"3.14"` to a floating-point number.
+a = "3.14"
+a = 3.14
+type(a)
+
+# Find the index of the first occurrence of the letter "o" in the string `"Python"`.
+name = "python"
+name("o")
+
+
+
+
+
+
+
+# Check if the substring "world" is present in the string `"Hello, World!"` using the `in` keyword.
+word ="Hello, World"
+name ="World"
+print(world in word)
